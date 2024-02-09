4964eaac7448f42179a9b9d0a6d87664de023844
Meha555 <wdsjhzy@163.com>
2024-02-09 19:55:00 +0800

    完成到iterators

---
+++ exercises/generics/generics1.rs
@@ -0,0 +1,14 @@
+// generics1.rs
+//
+// This shopping list program isn't compiling! Use your knowledge of generics to
+// fix it.
+//
+// Execute `rustlings hint generics1` or use the `hint` watch subcommand for a
+// hint.
+
+fn main() {
+    // let mut shopping_list  = Vec::<&str>::new();
+    // let mut shopping_list: Vec<&str> = Vec::new();
+    let mut shopping_list = Vec::new();
+    shopping_list.push("milk");
+}
