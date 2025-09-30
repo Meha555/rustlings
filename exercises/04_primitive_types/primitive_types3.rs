2dca42ec4f5b859bbbfbaa72b9544b978bbe7398
Meha555 <wdsjhzy@163.com>
2025-09-30 18:27:08 +0800

    第一次练习

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,9 +1,12 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
-    // let a = ???
+    let a = ["Are we there yet?"; 100];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
+        for i in 0..a.len() {
+            println!("a[{}] = {}", i, a[i]);
+        }
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
         panic!("Array not big enough, more elements needed");
